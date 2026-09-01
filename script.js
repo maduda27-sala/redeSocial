4eb26e0b42f898bdfcb3a65bc3fc6d4b2ab6ccf6
maduda27-sala <maducb07@gmail.com>
2026-09-01 09:02:56 -0300

    ok

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => (
+    const likeBtn = document.querySelector(".like-btn");
+    const postMedia = document.querySelector(".postmedia")
+if (!likeBtn) return;
+
+const likeCountSpan = likeBtn.querySelector(".like-count");
+const bookmarkBtn = document.querySelector(".bookmark-btn");
+
+
+
+
+
+
+
+
+))
